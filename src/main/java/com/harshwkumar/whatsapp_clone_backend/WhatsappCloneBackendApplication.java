17dcbf22e51fa9a1736fdfe88d0b108a45dbc9c4
Harshwardhan Kumar <harshwkumar@microsoft.com>
2024-12-18 19:01:35 +0530

    Initial commit

---
+++ src/main/java/com/harshwkumar/whatsapp_clone_backend/WhatsappCloneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.harshwkumar.whatsapp_clone_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsappCloneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsappCloneBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harshwkumar/whatsapp_clone_backend/WhatsappCloneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harshwkumar.whatsapp_clone_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsappCloneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
